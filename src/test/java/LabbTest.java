28424a86457e4f4901c3a143d83af4bcad0720de
Mattias <mattias.hovde@iths.se>
2021-03-24 15:12:09 +0100

    Final push

--- src/test/java/LabbTest.java
+++ src/test/java/LabbTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 // Labb TDD
 
-public class Labb1Test {
+public class LabbTest {
 
     Company company = new Company();
 
